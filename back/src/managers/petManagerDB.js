4f550b5d8c9d5999fec6cac27e3b2c39e29b2037
Ulises-Rodriguez <96356752+Ulises-Rodriguez@users.noreply.github.com>
2024-07-09 16:46:44 -0300

    manager de user listo, falta probarlo cuando este la db andando

--- back/src/managers/petManagerDB.js
+++ back/src/managers/petManagerDB.js
@@ -1,20 +1,20 @@
 import PetModel from '../models/PetModel.js';
 
 class Pet {
-    static getPets = async(req,res)=>{
+    static getPets = async (req, res) => {
         try {
             const pets = await PetModel.find();
 
             if (!pets) {
                 return res.status(500).send({
-                    status : "error",
-                    payload : "No se logro obtener todas las mascotas"
+                    status: "error",
+                    payload: "No se logro obtener todas las mascotas"
                 })
             }
 
             res.send({
-                status : "success",
-                payload : pets
+                status: "success",
+                payload: pets
             })
 
         } catch (error) {
@@ -22,100 +22,100 @@ class Pet {
         }
     }
 
-    static getPetById = async(req,res)=>{
+    static getPetById = async (req, res) => {
         try {
             const id = req.params.id;
             // const {idPet} = req.body; esto dependera por donde se mande el id de la mascota
 
-            const pet = await PetModel.findOne({id}); //puede ser q tengas q pasar el objeto como {_id : id}
+            const pet = await PetModel.findOne({ id }); //puede ser q tengas q pasar el objeto como {_id : id}
 
             if (!pet) {
                 return res.status(500).send({
-                    status : "error",
-                    payload : `No se logro obtener la mascota con el ID: ${id}`
+                    status: "error",
+                    payload: `No se logro obtener la mascota con el ID: ${id}`
                 })
             }
 
             res.send({
-                status : "success",
-                payload : pet
-            })            
+                status: "success",
+                payload: pet
+            })
 
         } catch (error) {
             console.log(error);
         }
     }
 
-    static createPet = async(req,res)=>{
+    static createPet = async (req, res) => {
         try {
             const newPet = await PetModel.create({});
 
             if (!newPet) {
                 return res.status(500).send({
-                    status : "error",
-                    payload : "Error al intentar añadir un nuevo animalito a la DB"
+                    status: "error",
+                    payload: "Error al intentar añadir un nuevo animalito a la DB"
                 })
             }
 
             res.send({
-                status : "success",
-                payload : newPet
-            })  
+                status: "success",
+                payload: newPet
+            })
 
         } catch (error) {
             console.log(error);
         }
     }
 
-    static updatePet = async(req,res)=>{
+    static updatePet = async (req, res) => {
         try {
             const id = req.params.id;
             // const {idPet} = req.body; 
-            
-            const filter = {id}; //puede ser q tengas q pasar el objeto como {_id : id}
-            const update = {...req.body};
 
-            const updatedPet = await PetModel.findOneAndUpdate(filter,update);
+            const filter = { id }; //puede ser q tengas q pasar el objeto como {_id : id}
+            const update = { ...req.body };
+
+            const updatedPet = await PetModel.findOneAndUpdate(filter, update);
 
             if (!updatedPet) {
                 return res.status(500).send({
-                    status : "error",
-                    payload : `No se logro actualizar el animalito con el ID: ${id}`
+                    status: "error",
+                    payload: `No se logro actualizar el animalito con el ID: ${id}`
                 })
             }
 
             res.send({
-                status : "success",
-                payload : updatedPet
-            }) 
+                status: "success",
+                payload: updatedPet
+            })
 
         } catch (error) {
             console.log(error);
         }
     }
 
-    static deletePet = async(req,res)=>{
+    static deletePet = async (req, res) => {
         try {
             const id = req.params.id;
 
-            const deletedPet = await PetModel.deleteOne({id}); //puede ser q tengas q pasar el objeto como {_id : id}
+            const deletedPet = await PetModel.deleteOne({ id }); //puede ser q tengas q pasar el objeto como {_id : id}
 
             if (!deletedPet) {
                 return res.status(500).send({
-                    status : "error",
-                    payload : `No se logro actualizar el animalito con el ID: ${id}`
+                    status: "error",
+                    payload: `No se logro actualizar el animalito con el ID: ${id}`
                 })
             }
 
             res.send({
-                status : "success",
-                payload : updatedPet
-            }) 
-            
+                status: "success",
+                payload: deletedPet
+            })
+
         } catch (error) {
             console.log(error);
         }
     }
 }
 
-export {Pet}
+export { Pet }
--- back/src/managers/userManagerDB.js
+++ back/src/managers/userManagerDB.js
@@ -1,29 +1,116 @@
-import UserModel from '../models/UserModel.js';
+import UserModel from "../models/UserModel.js";
 
-class User{
-
-    //2 formas de hacerlo podemos usar static para no tener q hacer la instancia
-    static getUsers = async()=>{
-        try {
-            const users = await UserModel.find(); //me falta ver el tema de PRE y POPULATE (para las referencias con otros documentos)
-
-            return users;
-        } catch (error) {
-            console.log(error);
-        }
-    }
-
-    //o podemos hacerlo asi y tener q crear la instancia 
-    //const UserInstance = new User();
-    getUsers2 = async()=>{
+class User {
+    static getUsers = async (req, res) => {
         try {
             const users = await UserModel.find();
 
-            return users;
+            if (!users) {
+                return res.status(500).send({
+                    status: "error",
+                    payload: "No se logro obtener todos los usuarios",
+                });
+            }
+
+            res.send({
+                status: "success",
+                payload: users,
+            });
         } catch (error) {
             console.log(error);
         }
-    }
+    };
+
+    static getUserById = async (req, res) => {
+        try {
+            const id = req.params.id;
+            // const {idUser} = req.body; esto dependera por donde se mande el id de la mascota
+
+            const user = await UserModel.findOne({ id }); //puede ser q tengas q pasar el objeto como {_id : id}
+
+            if (!user) {
+                return res.status(500).send({
+                    status: "error",
+                    payload: `No se logro obtener al usuario con el ID: ${id}`,
+                });
+            }
+
+            res.send({
+                status: "success",
+                payload: user,
+            });
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
+    static createPet = async (req, res) => {
+        try {
+            const newUser = await UserModel.create({});
+
+            if (!newUser) {
+                return res.status(500).send({
+                    status: "error",
+                    payload: "Error al intentar añadir un nuevo usuario a la DB",
+                });
+            }
+
+            res.send({
+                status: "success",
+                payload: newUser,
+            });
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
+    static updateUser = async (req, res) => {
+        try {
+            const id = req.params.id;
+            // const {idPet} = req.body;
+
+            const filter = { id }; //puede ser q tengas q pasar el objeto como {_id : id}
+            const update = { ...req.body };
+
+            const updatedUser = await UserModel.findOneAndUpdate(filter, update);
+
+            if (!updatedUser) {
+                return res.status(500).send({
+                    status: "error",
+                    payload: `No se logro actualizar el usuario con el ID: ${id}`,
+                });
+            }
+
+            res.send({
+                status: "success",
+                payload: updatedUser,
+            });
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
+    static deletePet = async (req, res) => {
+        try {
+            const id = req.params.id;
+
+            const deletedUser = await UserModel.deleteOne({ id }); //puede ser q tengas q pasar el objeto como {_id : id}
+
+            if (!deletedUser) {
+                return res.status(500).send({
+                    status: "error",
+                    payload: `No se logro actualizar el animalito con el ID: ${id}`,
+                });
+            }
+
+            res.send({
+                status: "success",
+                payload: deletedUser,
+            });
+        } catch (error) {
+            console.log(error);
+        }
+    };
 }
 
-export {User};
+export { User };
