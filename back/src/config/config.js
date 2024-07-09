ab91b0167e24e6d3fc292546290e0c9e32e530f1
Ulises-Rodriguez <96356752+Ulises-Rodriguez@users.noreply.github.com>
2024-07-09 18:07:20 -0300

    añadiendo la funcionalidad para enviar emails y dotenv para las vairbales de entorno

--- back/src/config/config.js
+++ back/src/config/config.js
@@ -6,10 +6,12 @@ const PORT = process.env.PORT;
 const DATABASE_URL = process.env.DATABASE_URL;
 const ADMIN_EMAIL = process.env.ADMIN_EMAIL;
 const ADMIN_PASSWORD = process.env.ADMIN_PASSWORD;
+const ADMIN_PASSWORD_NODEMAILER = process.env.ADMIN_PASSWORD_NODEMAILER;
 
 export const options = {
     PORT,
     DATABASE_URL,
     ADMIN_EMAIL,
     ADMIN_PASSWORD,
+    ADMIN_PASSWORD_NODEMAILER,
 }
---
+++ back/src/utils/emailService.js
@@ -0,0 +1,38 @@
+import nodemailer from 'nodemailer';
+import {options} from '../config/config.js';
+
+// PetMatchNoCountry123
+const ADMIN_EMAIL = process.env.ADMIN_EMAIL || options.ADMIN_EMAIL;
+const ADMIN_PASSWORD_NODEMAILER = process.env.ADMIN_PASSWORD_NODEMAILER || options.ADMIN_PASSWORD_NODEMAILER;
+
+const transporter = nodemailer.createTransport({
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false, // Use `true` for port 465, `false` for all other ports
+    auth: {
+        user: ADMIN_EMAIL,
+        pass: ADMIN_PASSWORD_NODEMAILER,
+    },
+    tls: {
+        rejectUnauthorized: false
+    }
+});
+
+export const emailSender = async (email, template, subject = "Atencion al cliente") => {
+    try {
+        const contenido = await transporter.sendMail({
+            //Estructura del correo
+            from: "PetMatch",
+            to: email,
+            subject,
+            html: template
+        })
+
+        return true;
+
+    } catch (error) {
+        console.log(error.message);
+
+        return false;
+    }
+}
