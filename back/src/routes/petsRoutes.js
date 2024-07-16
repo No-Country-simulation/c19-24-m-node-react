dadef4a26ded1cce7a18b2c06e415b9155c5eb06
Ulises <uliisesrodriguez809@gmail.com>
2024-07-16 18:26:21 -0300

    probando los enpoints y error client

--- back/src/routes/petsRoutes.js
+++ back/src/routes/petsRoutes.js
@@ -1,21 +1,21 @@
-import {Router} from 'express'
+import { Router } from 'express'
 import { PetController } from '../controllers/petController.js';
 
 const router = Router();
 
 //obtener todos las mascotasª
-router.get('/', PetController.getPets)
+router.get('/', PetController.getPets);
 
 //obtener mascota por id
-router.get('/:id', PetController.getPetById)
+router.get('/:id', PetController.getPetById);
 
 //Crear una nueva mascota
-router.post('/', PetController.createPet)
+router.post('/', PetController.createPet);
 
 //Eliminar mascota por id
-router.delete('/:id', PetController.deletePet)
+router.delete('/:id', PetController.deletePet);
 
 //Actualizar mascota por id
-router.put('/:id', PetController.updatePet)
+router.put('/:id', PetController.updatePet);
 
 export default router;
