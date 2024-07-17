96775afe0f130db89a513b1adff82aca01365015
Simón Moreno <75553043+Simon-M20@users.noreply.github.com>
2024-07-17 09:33:54 -0300

    adding responsive design

--- front/petmatch/src/Components/Footer/Footer.jsx
+++ front/petmatch/src/Components/Footer/Footer.jsx
@@ -12,10 +12,10 @@ function Footer() {
     };
 
     return (
-        <footer className={`${styles.footer} px-8 pt-16 pb-4`}>
+        <footer className={`${styles.footer} md:px-8 px-4 md:pt-16 pt-8 pb-4`}>
             <section>
                 <form
-                    className={`${styles.footer__form} lg:flex-nowrap flex-wrap px-8 py-8 lg:pt-8 md:pt-5 rounded-2xl w-full flex items-center lg:justify-between justify-center`}
+                    className={`${styles.footer__form} lg:flex-nowrap flex-wrap md:px-8 px-4 py-8 lg:pt-8 md:pt-5 rounded-2xl w-full flex items-center lg:justify-between justify-center`}
                     onSubmit={handleSubmit}>
                     <div className='xl:w-1/3 lg:w-2/5 w-full lg:pb-0 pb-5 lg:text-left text-center'>
                         <label
@@ -24,23 +24,23 @@ function Footer() {
                             Regístrate ahora para no perderte Novedades
                         </label>
                     </div>
-                    <div className='lg:w-2/3 w-full bg-white rounded-xl gap-x-5 lg:p-3.5 p-2 flex items-center justify-between'>
+                    <div className='lg:w-2/3 w-full bg-white rounded-xl md:gap-x-5 gap-x-2 lg:p-3.5 p-2 flex items-center justify-between'>
                         <input
                             id='Subscribete'
                             type='text'
                             placeholder='Ingresa tu correo electrónico'
-                            className='outline-gray-400 outline-1 w-4/5 lg:py-3 py-2 rounded-xl px-5'
+                            className='outline-gray-400 outline-1 md:w-4/5 w-full lg:py-3 py-2 rounded-xl px-5'
                         />
                         <button
-                            className='rounded-xl lg:py-3 py-2 px-10 text-white'
+                            className='rounded-xl lg:py-3 py-2 md:px-10 px-5 text-white'
                             style={{ backgroundColor: "#416A32" }}>
                             ¡Suscríbete!
                         </button>
                     </div>
                 </form>
                 <section className='w-full flex justify-center items-center pb-7 pt-8 px-3'>
-                    <nav className='w-full flex justify-between items-center'>
-                        <ul className='w-1/2 flex justify-start lg:gap-x-12 md:gap-x-6 items-center'>
+                    <nav className='w-full flex justify-between items-center md:flex-nowrap flex-wrap'>
+                        <ul className='md:w-1/2 w-full flex md:justify-start justify-center lg:gap-x-12 gap-x-6 items-center'>
                             <li
                                 className='font-bold lg:text-base text-sm cursor-pointer'
                                 style={{ color: "var(--main-color)" }}>
@@ -67,7 +67,7 @@ function Footer() {
                                 Blog
                             </li>
                         </ul>
-                        <ul className='w-1/3 flex items-center justify-end lg:gap-x-12 md:gap-x-6'>
+                        <ul className='md:w-1/3 w-full flex items-center md:justify-end justify-center lg:gap-x-12 gap-x-6 md:pt-0 pt-5'>
                             <li className='cursor-pointer' title='Facebook'>
                                 <img src={Facebook} alt='Facebook Icon' />
                             </li>
@@ -85,7 +85,7 @@ function Footer() {
                 </section>
             </section>
             <hr className='w-full mx-auto mt-2' />
-            <section className='flex justify-between items-center lg:py-8 py-4'>
+            <section className='flex md:justify-between justify-center items-center lg:py-8 py-4 md:flex-nowrap flex-wrap'>
                 <p
                     className='pt-2 xl:text-base lg:text-sm text-xs'
                     style={{ color: "#667479" }}>
@@ -94,7 +94,7 @@ function Footer() {
                 <figure className='lg:block hidden'>
                     <img src={Logo} alt='PetMatch Logo' />
                 </figure>
-                <div className='flex justify-between items-center lg:gap-x-8 gap-x-2'>
+                <div className='flex justify-between items-center lg:gap-x-8 md:gap-x-2 gap-x-8 md:mt-0 mt-4'>
                     <p
                         className='pt-1 xl:text-base lg:text-sm text-xs'
                         style={{ color: "#667479" }}>
