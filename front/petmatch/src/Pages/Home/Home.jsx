b0ed054e8775aa29518f81b0866ab5672ff7c3d8
fhuayller <fhuayller@gmail.com>
2024-07-18 11:18:29 -0300

    HP with different measures

--- front/petmatch/src/Pages/Home/Home.jsx
+++ front/petmatch/src/Pages/Home/Home.jsx
@@ -13,20 +13,20 @@ function Home() {
           className="bg-contain bg-no-repeat bg-center min-h-screen flex flex-col justify-between"
           style={{ backgroundImage: `url(${fondo})` }}
         >
-          <section className="mt-52 max-w-3xl mx-32 text-left">
-            <h1 className="text-6xl font-bold text-bgGreen">
+          <section className="mt-20 sm:mt-32 md:mt-40 lg:mt-52 max-w-xl sm:max-w-2xl md:max-w-3xl mx-4 sm:mx-12 md:mx-24 lg:mx-32 text-left">
+            <h1 className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl font-bold text-bgGreen">
               ¡Swipe, Match Y Adopta Con PetMatch!
             </h1>
-            <p className="mt-4 text-lg text-black font-semibold">
+            <p className="mt-4 text-base sm:text-lg md:text-xl lg:text-lg text-black font-semibold">
               Adopta adorables mascotas que te acompañarán fielmente, te
               apoyarán y alegrarán cada día. ¡Empieza hoy y dale a una mascota
               la oportunidad de ser parte de tu familia!
             </p>
-            <div className="mt-6 space-x-4">
-              <button className="bg-white text-bgGreen border border-bgGreen rounded-full px-6 py-2">
+            <div className="mt-6 space-x-2 sm:space-x-4">
+              <button className="bg-white text-bgGreen border border-bgGreen rounded-full px-4 sm:px-6 py-2">
                 ¡Haz Match!
               </button>
-              <button className="bg-bgGreen text-white rounded-full px-6 py-2">
+              <button className="bg-bgGreen text-white rounded-full px-4 sm:px-6 py-2">
                 Ver Catálogo
               </button>
             </div>
@@ -38,12 +38,12 @@ function Home() {
       </main>
       <section className="bg-white bg-contain bg-no-repeat">
         <img src={huella} alt="huella de perro" />
-        <div className="flex items-center justify-between mb-20">
-          <div className="flex-auto p-10 mt-12 max-w-3xl mx-32 text-left">
-            <h1 className="text-6xl font-bold text-bgGreen">
+        <div className="flex flex-col md:flex-row items-center justify-between mb-20">
+          <div className="flex-auto p-4 sm:p-10 mt-6 sm:mt-12 max-w-xl sm:max-w-2xl md:max-w-3xl mx-4 sm:mx-12 md:mx-24 lg:mx-32 text-left">
+            <h1 className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl font-bold text-bgGreen">
               Acerca De Nosotros
             </h1>
-            <p className="mt-4 text-lg text-black font-semibold">
+            <p className="mt-4 text-base sm:text-lg md:text-xl lg:text-lg text-black font-semibold">
               Somos un grupo de personas que nos enfocamos en salvar a nuestros
               amigos peludos, cuidarlos y alimentarlos. Estamos comprometidos en
               encontrarles un hogar para que puedan ser felices y tengan una
@@ -53,16 +53,16 @@ function Home() {
               ¡Conócenos!
             </button>
           </div>
-          <div className="flex-shrink-0 mr-32">
-            <img src={perrito} className=" mr-6" alt="perro front" />
+          <div className="flex-shrink-0 mx-4 sm:mx-12 md:mx-24 lg:mx-32 mt-6 md:mt-0">
+            <img src={perrito} className="max-w-full" alt="perro front" />
           </div>
         </div>
       </section>
       <section>
-        <div className="flex items-start justify-between p-6 bg-bgPrincipal">
-          <div className="flex-auto p-10 mt-12 max-w-3xl mx-32 text-left">
-            <h1 className="text-6xl font-bold text-bgGreen">Contáctanos</h1>
-            <p className="mt-4 text-gray-700">
+        <div className="flex flex-col md:flex-row items-start justify-between p-4 sm:p-6 bg-bgPrincipal">
+          <div className="flex-auto p-4 sm:p-10 mt-6 sm:mt-12 max-w-xl sm:max-w-2xl md:max-w-3xl mx-4 sm:mx-12 md:mx-24 lg:mx-32 text-left">
+            <h1 className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl font-bold text-bgGreen">Contáctanos</h1>
+            <p className="mt-4 text-base sm:text-lg md:text-xl lg:text-lg text-gray-700">
               ¿Quieres adoptar una mascota o formar parte del equipo? Llena el
               siguiente formulario y nos pondremos en contacto para responderte
               a la brevedad.
@@ -74,7 +74,7 @@ function Home() {
               ¡Conócenos!
             </button>
           </div>
-          <div className="flex-1 ml-6 mr-32">
+          <div className="flex-1 p-4 sm:p-10 mx-4 sm:mx-12 md:mx-24 lg:mx-32">
             <form className="space-y-4">
               <input
                 type="text"
