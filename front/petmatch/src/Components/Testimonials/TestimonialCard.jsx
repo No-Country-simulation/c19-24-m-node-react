98ac64b0001acc2bd94cdc0c4f8248e2e55a7a4d
Simón Moreno <75553043+Simon-M20@users.noreply.github.com>
2024-07-19 15:06:51 -0300

    changing width and height

--- front/petmatch/src/Components/Testimonials/TestimonialCard.jsx
+++ front/petmatch/src/Components/Testimonials/TestimonialCard.jsx
@@ -1,15 +1,19 @@
 function TestimonialCard({ owner, testimonial, pet, img }) {
     return (
-        <figure className='w-[370px] bg-white rounded-2xl'>
+        <figure className='w-[320px] bg-white rounded-2xl'>
             <img
-                className='w-full object-cover rounded-t-2xl'
+                className='w-full h-[200px] object-cover object-top rounded-t-2xl'
                 src={img}
                 alt={`${owner} - ${pet}`}
+                width={"100%"}
+                height={"100%"}
             />
-            <figcaption className='p-4 flex flex-col text-[#141414]'>
+            <figcaption className='justify-between p-4 items-start h-[200px]  flex flex-col text-[#141414]'>
                 {testimonial}
-                <span className='pt-2 font-bold'>{owner}</span>
-                <small className='text-gray-500'>{pet}</small>
+                <span className='pt-2 font-bold flex flex-col'>
+                    {owner}
+                    <small className='text-gray-500'>{pet}</small>
+                </span>
             </figcaption>
         </figure>
     );
