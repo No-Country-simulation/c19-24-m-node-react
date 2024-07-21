c8de6f4f1d18fbe00d2b87600f3b12a059f6cf89
Simón Moreno <75553043+Simon-M20@users.noreply.github.com>
2024-07-21 15:36:26 -0300

    adding responsive design

--- front/petmatch/src/Components/Testimonials/TestimonialCard.jsx
+++ front/petmatch/src/Components/Testimonials/TestimonialCard.jsx
@@ -1,15 +1,15 @@
 function TestimonialCard({ owner, testimonial, pet, img }) {
     return (
-        <figure className='w-[320px] bg-white rounded-2xl'>
+        <figure className='w-[320px] bg-white md:rounded-2xl rounded-lg'>
             <img
-                className='w-full h-[200px] object-cover object-top rounded-t-2xl'
+                className='w-full md:h-[200px] h-[280px] object-cover object-top md:rounded-t-2xl rounded-t-lg'
                 src={img}
                 alt={`${owner} - ${pet}`}
                 width={"100%"}
                 height={"100%"}
             />
-            <figcaption className='justify-between p-4 items-start h-[200px]  flex flex-col text-[#141414]'>
-                {testimonial}
+            <figcaption className='justify-between p-5 items-start md:h-[200px] h-[220px] flex flex-col text-[#141414]'>
+                <p className='md:line-clamp-4 line-clamp-5'>{testimonial}</p>
                 <span className='pt-2 font-bold flex flex-col'>
                     {owner}
                     <small className='text-gray-500'>{pet}</small>
