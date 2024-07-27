1de229e02e670009c4fafbae3558860a1f3e2db6
Simón Moreno <75553043+Simon-M20@users.noreply.github.com>
2024-07-27 19:33:19 -0300

    Update Routes.jsx

--- front/petmatch/src/Routes/Routes.jsx
+++ front/petmatch/src/Routes/Routes.jsx
@@ -6,8 +6,16 @@ import Matches from "../Pages/Matches/Matches";
 import MatchPage from "../Pages/matchPage/MatchPage";
 import ComingSoon from "../Pages/Comming Soon/CommingSoon";
 import Catalogue from "../Pages/Catalogue/Catalogue";
+import MyAccount from "../Pages/Auth/MyAccount";
+import PetsContext from "../Context/GlobalContext";
+import { useContext } from "react";
+import Loader from "../Components/Loading";
 
 function AppRoutes() {
+    const { loading } = useContext(PetsContext);
+
+    if (loading) return <Loader />;
+
     return (
         <Routes>
             <Route path='*' element={<ComingSoon />} />
@@ -16,6 +24,9 @@ function AppRoutes() {
             <Route path='/Match' element={<MatchPage />} />
             <Route path='/Sign-Up' element={<RegisterPage />} />
             <Route path='/Log-In' element={<LoginPage />} />
+            {/* <Route path='/Account-Settings/:userId' element={<AccountSettings />} />  */}
+            <Route path='/Account-Settings' element={<MyAccount />} />
+
             <Route path='/Catalogue' element={<Catalogue />} />
             <Route path='/About-Us' element={<ComingSoon />} />
             <Route path='/Admin' element={<ComingSoon />} />
