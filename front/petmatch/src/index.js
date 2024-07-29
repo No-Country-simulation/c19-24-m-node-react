b47ade643e59b1a888d5aec29274faf5c7b5f202
Simón Moreno <75553043+Simon-M20@users.noreply.github.com>
2024-07-28 22:15:50 -0300

    Update index.js

--- front/petmatch/src/index.js
+++ front/petmatch/src/index.js
@@ -7,11 +7,11 @@ import { PetsProvider } from "./Context/GlobalContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-    <React.StrictMode>
-        <PetsProvider>
-            <App />
-        </PetsProvider>
-    </React.StrictMode>
+    // <React.StrictMode>
+    <PetsProvider>
+        <App />
+    </PetsProvider>
+    // </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
