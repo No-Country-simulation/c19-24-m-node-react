61ee43f015411e631fd2f2c0b5c682ef50d06e09
Simón Moreno <75553043+Simon-M20@users.noreply.github.com>
2024-07-28 23:10:34 -0300

    fixed issue with the API url

--- front/petmatch/src/Helpers/API.js
+++ front/petmatch/src/Helpers/API.js
@@ -2,7 +2,8 @@ const baseURL = "https://c19-24-m-node.onrender.com";
 
 export const getPets = async () => {
     try {
-        let url = `${baseURL}/pets`;
+        // let url = `${baseURL}/pets`;
+        let url = `http://localhost:8080/pets/petsQuantity/16`;
         const res = await fetch(url);
         const data = await res.json();
         if (data.status === "success") return data.payload;
